1497fa736245fa30903bfeafa14be86b8640cb79
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-29 21:14:01 +0530

    eBookStore-Eureka-Server

---
+++ eBookStore-Eureka-Server/eBookStore-Eureka-Server/src/main/java/com/example/eBookStoreEurekaServer/EBookStoreEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.eBookStoreEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EBookStoreEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookStoreEurekaServerApplication.class, args);
+	}
+
+}
---
+++ eBookStore-Eureka-Server/eBookStore-Eureka-Server/src/test/java/com/example/eBookStoreEurekaServer/EBookStoreEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eBookStoreEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookStoreEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
