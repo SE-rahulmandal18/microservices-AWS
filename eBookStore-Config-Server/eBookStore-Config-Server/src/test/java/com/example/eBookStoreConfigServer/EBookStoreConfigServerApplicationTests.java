2ccfe80346fad9632b1e131a9dd1920182136d64
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-29 21:11:08 +0530

    eBookStore-Config-Server

---
+++ eBookStore-Config-Server/eBookStore-Config-Server/src/test/java/com/example/eBookStoreConfigServer/EBookStoreConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eBookStoreConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookStoreConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
