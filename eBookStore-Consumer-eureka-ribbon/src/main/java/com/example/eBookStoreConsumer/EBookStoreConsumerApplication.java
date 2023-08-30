82231cb8398fe9dafeeddec50d439bc884d81853
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-30 14:18:23 +0530

    Enable Dynamic Discovery to eBookStore Consumer Microserviceusing Ribbon

---
+++ eBookStore-Consumer-eureka-ribbon/src/main/java/com/example/eBookStoreConsumer/EBookStoreConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eBookStoreConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookStoreConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookStoreConsumerApplication.class, args);
+	}
+
+}
---
+++ eBookStore-Consumer-eureka-ribbon/src/main/java/com/example/eBookStoreConsumer/controller/BookClientController.java
@@ -0,0 +1,33 @@
+package com.example.eBookStoreConsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.eBookStoreConsumer.entity.Book;
+
+@RestController
+@Scope("request")
+public class BookClientController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	@GetMapping("/get-books")
+	public Book getBooks()
+	{
+		Book book = restTemplate.getForObject("http://book-service/books/", Book.class);
+		return book;
+	}
+	
+	@GetMapping("/get-books/{id}")
+	public Book getBookById(@PathVariable("id") int id)
+	{
+		Book book = restTemplate.getForObject("http://book-service/books/" + id, Book.class);
+		return book;
+	}
+}
