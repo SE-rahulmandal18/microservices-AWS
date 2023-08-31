711e22974271a0be41d7790fbe1f04f41684511f
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-31 14:09:58 +0530

    implementing Circuit Breaker using Resilience4J on Feign Consumer

--- eBookStore-Consumer-Eureka-Feign-Resilience4J/src/main/java/com/example/eBookStoreConsumerFeign/proxy/BookServiceProxy.java
+++ eBookStore-Consumer-Eureka-Feign-Resilience4J/src/main/java/com/example/eBookStoreConsumerFeign/proxy/BookServiceProxy.java
@@ -3,23 +3,28 @@ package com.example.eBookStoreConsumerFeign.proxy;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.eBookStoreConsumerFeign.entity.Book;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name="book-service")
 public interface BookServiceProxy {
-	
-	@GetMapping("/books")
-	public List<Book> getAllBooks() ;
 
-	// Get a book by its ID
+
+	@CircuitBreaker(name = "book-service", fallbackMethod = "fallbackMethodgetBookById")
 	@GetMapping("/books/{id}")
 	public Book getBookById(@PathVariable("id")int id);
 	
+	
+	@CircuitBreaker(name = "book-service", fallbackMethod = "fallbackMethodgetAllBooks")
+	@GetMapping("/books")
+	public List<Book> getAllBooks() ;
+	
+
 	
 	public default List<Book> fallbackMethodgetAllBooks(Throwable cause) {
 		System.out.println("Exception Raised with the message:===>" + cause.getMessage());
