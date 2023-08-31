3b398cb3a61a122ccab45a2849a31742aa19c876
Rahul Mandal <rahulbokaro610@gmail.com>
2023-08-31 15:05:50 +0530

    implementing Circuit Breaker using Resilience4J on Feign Consumer

--- eBookStore-Consumer-Eureka-Feign-Resilience4J/src/main/java/com/example/eBookStoreConsumerFeign/proxy/BookServiceProxy.java
+++ eBookStore-Consumer-Eureka-Feign-Resilience4J/src/main/java/com/example/eBookStoreConsumerFeign/proxy/BookServiceProxy.java
@@ -10,16 +10,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.example.eBookStoreConsumerFeign.entity.Book;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @FeignClient(name="book-service")
 public interface BookServiceProxy {
 
-
+    @Retry(name="book-service")
 	@CircuitBreaker(name = "book-service", fallbackMethod = "fallbackMethodgetBookById")
 	@GetMapping("/books/{id}")
 	public Book getBookById(@PathVariable("id")int id);
 	
-	
+    @Retry(name="book-service")
 	@CircuitBreaker(name = "book-service", fallbackMethod = "fallbackMethodgetAllBooks")
 	@GetMapping("/books")
 	public List<Book> getAllBooks() ;
