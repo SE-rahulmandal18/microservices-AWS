f61860ba2c39ea79e53a0c8b0d9e0275cc5849cc
Rahul Mandal <rahulbokaro610@gmail.com>
2023-09-01 10:40:33 +0530

    prometheus & micrometer

---
+++ eBookStore-Consumer-Eureka-Ribbon-Resilence4J-MP/src/main/java/com/example/eBookStoreConsumer/EBookStoreConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.example.eBookStoreConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class EBookStoreConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookStoreConsumerApplication.class, args);
+	}
+
+}
