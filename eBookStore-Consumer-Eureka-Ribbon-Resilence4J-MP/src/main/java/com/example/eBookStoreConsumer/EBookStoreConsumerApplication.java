defb26febc03b4c68f8e0dc925ba7b1f225cc95b
Rahul Mandal <rahulbokaro610@gmail.com>
2023-09-01 11:57:35 +0530

    Zipkin and Sleuth Integration with Ribbon Consumer Application

--- eBookStore-Consumer-Eureka-Ribbon-Resilence4J-MP/src/main/java/com/example/eBookStoreConsumer/EBookStoreConsumerApplication.java
+++ eBookStore-Consumer-Eureka-Ribbon-Resilence4J-MP/src/main/java/com/example/eBookStoreConsumer/EBookStoreConsumerApplication.java
@@ -2,7 +2,10 @@ package com.example.eBookStoreConsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class EBookStoreConsumerApplication {
 		SpringApplication.run(EBookStoreConsumerApplication.class, args);
 	}
 
+	@Bean
+	public TimedAspect timeAspect(MeterRegistry registry) {
+		
+		return new TimedAspect(registry);
+	}
 }
--- eBookStore-Consumer-Eureka-Ribbon-Resilence4J-MP/src/main/java/com/example/eBookStoreConsumer/controller/BookClientController.java
+++ eBookStore-Consumer-Eureka-Ribbon-Resilence4J-MP/src/main/java/com/example/eBookStoreConsumer/controller/BookClientController.java
@@ -1,15 +1,15 @@
 package com.example.eBookStoreConsumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.example.eBookStoreConsumer.entity.Book;
 import com.example.eBookStoreConsumer.service.BookService;
+
+import io.micrometer.core.annotation.Timed;
 
 @RestController
 @Scope("request")
@@ -18,7 +18,8 @@ public class BookClientController {
 	@Autowired
 	private BookService bookService;
 	
-	
+
+	@Timed(value = "getBookById.time", description = "Taken time to return book")
 	@GetMapping("/get-books/{id}")
 	public Book getBookById(@PathVariable("id")int id)
 	{
