b56737f0bfd7bd391d4d9b7b67edbf833ab84228
Rahul Mandal <rahulbokaro610@gmail.com>
2023-09-02 10:55:11 +0530

    gateway filterFactory

--- eBookStore-Cloud-Gateway-API/src/main/java/com/example/demo/EBookStoreProxyApiApplication.java
+++ eBookStore-Cloud-Gateway-API/src/main/java/com/example/demo/EBookStoreProxyApiApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
-public class EBookStoreProxyApi1Application {
+public class EBookStoreProxyApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EBookStoreProxyApi1Application.class, args);
+		SpringApplication.run(EBookStoreProxyApiApplication.class, args);
 	}
 
 }
